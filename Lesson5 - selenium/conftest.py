3f790b3ab7c08b87c0e7c256141d51bc2d33873a
Viktoriya <araslanovaviktoria@gmail.com>
2021-03-13 23:17:26 +0500

    fix dz_intro_in_selenium 1) убрала лишние пробелы 2) везде где assert сделала использование find_elements

--- Lesson5 - selenium/conftest.py
+++ Lesson5 - selenium/conftest.py
@@ -1,15 +1,25 @@
 import pytest
 from selenium import webdriver
 from msedge.selenium_tools import Edge, EdgeOptions
-import platform,os
+import platform
+import os
 
 
 def pytest_addoption(parser):
-    parser.addoption("--maximized", action="store_true", help="Maximize browser windows")
-    parser.addoption("--headless", action="store_true", help="Run headless")
-    parser.addoption("--browser", action="store", choices=["chrome", "firefox", "edge"],
-                     default="firefox", help="Browser")
-    parser.addoption("--url", default="https://demo.opencart.com/", help="Request base url")
+    parser.addoption("--maximized",
+                     action="store_true",
+                     help="Maximize browser windows")
+    parser.addoption("--headless",
+                     action="store_true",
+                     help="Run headless")
+    parser.addoption("--browser",
+                     action="store",
+                     choices=["chrome", "firefox", "edge"],
+                     default="firefox",
+                     help="Browser")
+    parser.addoption("--url",
+                     default="https://demo.opencart.com/",
+                     help="Request base url")
 
 
 @pytest.fixture(scope='module')
@@ -24,21 +34,28 @@ def browser(request):
     browser = request.config.getoption("--browser")
     if browser == "chrome":
         options = webdriver.ChromeOptions()
-        if headless:options.headless = True
+        if headless:
+            options.headless = True
         driver = webdriver.Chrome(options=options)
     if browser == "firefox":
         options = webdriver.FirefoxOptions()
         if platform.system() == "Windows":
             local_data = os.getenv('LOCALAPPDATA', None)
-            options.binary_location = os.path.join(local_data, r"Mozilla Firefox\firefox.exe")
-        if headless: options.headless = True
+            options.binary_location = os.path.join(
+                local_data,
+                r"Mozilla Firefox\firefox.exe")
+        if headless:
+            options.headless = True
         driver = webdriver.Firefox(options=options)
     if browser == "edge":
         options = EdgeOptions()
         if headless:
-            options.use_chromium = True;
+            options.use_chromium = True
             options.headless = True
-        driver = Edge(executable_path=r"C:/projects/drivers/MicrosoftWebDriver.exe", options=options)
+        driver = Edge(
+            executable_path=r"C:/projects/drivers/MicrosoftWebDriver.exe",
+            options=options
+        )
     if maximized:
         driver.maximize_window()
 
